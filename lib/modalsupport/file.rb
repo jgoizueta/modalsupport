979db42d5df9e3afb6d319333cd0ae575c1dc85f
Javier Goizueta <jgoizueta@gmail.com>
2010-06-09 12:03:06 +0200

    Remove ugly & incomplete fix for Rubinius File.relative_path

--- lib/modalsupport/file.rb
+++ lib/modalsupport/file.rb
@@ -1,12 +1,7 @@
 def File.relative_path(path, base=nil)
    base ||= '.'
    casefold = File::ALT_SEPARATOR=='\\'
-   # Note: currently Rubinius differs from MRI in that
-   # File.expand_path('//x') is '//x' in MRI and '/x'
-   # Since '//x' could be an absolute path, we should fix this here:   
-   double_slash = base.starts_with?('\\\\')
    base = File.expand_path(base)
-   base = '\\'+base if double_slash && !base.starts_with('\\\\')
    base = base.gsub(File::ALT_SEPARATOR, File::SEPARATOR) if File::ALT_SEPARATOR
    base.downcase! if casefold
    base += File::SEPARATOR unless base[-1,1]==File::SEPARATOR
