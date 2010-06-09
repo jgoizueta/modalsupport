a868f3fa9cd88bc421061ed25978fe8c4283e29e
Javier Goizueta <jgoizueta@gmail.com>
2010-06-09 17:14:00 +0200

    Version bump to 0.5.0

---
+++ lib/modalsupport/full.rb
@@ -0,0 +1,11 @@
+require 'modalsupport'
+begin
+  require 'active_support'
+rescue LoadError
+  puts "ActiveSupport not available. Install it with: gem install activesupport"
+end
+begin
+  require 'hobosupport'
+rescue LoadError
+  puts "HoboSupport not available. Install it with: gem install hobosupport"
+end
