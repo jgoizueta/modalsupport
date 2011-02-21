1fdc8a12a76b8f21b6b7189625fddf6beb7b99b2
Javier Goizueta <jgoizueta@gmail.com>
2011-02-21 22:28:32 +0100

    Add some comments to BracketConstructor.

--- lib/modalsupport/mixins/bracket_constructor.rb
+++ lib/modalsupport/mixins/bracket_constructor.rb
@@ -1,4 +1,26 @@
-# Mixin that adds a [] operator constructor to a class
+# Mixin that adds a [] operator constructor to a class, which
+# is a not-so-ugly alternative to the +new+ method.
+#
+#   class A
+#     include BracketConstructor
+#     def initialize(a)
+#       puts "A.new(#{a.inspect})"
+#     end 
+#   end
+#
+#   A[100] # => A.new(100)
+#
+# In Rails, this kind of constructor has an advantage over a function such as:
+#
+#   def A(*args)
+#     A.new(*args)
+#   end
+#
+# With the A[] notation we've referenced the class name via the constant A, 
+# which in Rails brings in the declaration for A automatically, whereas the
+# method A() won't be found unless the file which declares it is explicitely
+# required.
+#
 module ModalSupport::BracketConstructor
   
   def self.included(base)
