293209dfa6f31769739fcbb98fb2a37dca09404b
Javier Goizueta <jgoizueta@gmail.com>
2011-02-21 22:44:57 +0100

    Add test.

--- test/test_array_to_h.rb
+++ test/test_array_to_h.rb
@@ -6,6 +6,7 @@ class TestArrayIndex < Test::Unit::TestCase
 
     should "have an to_h method converting key-value pairs to a hash" do
       assert_respond_to [], :to_h
+      assert_equal({}, [].to_h)
       assert_equal({:a=>11, :b=>22}, [[:a,11], [:b,22]].to_h)
       assert_equal({:a=>11, :b=>[22,33]}, [[:a,11], [:b,22,33]].to_h)
       assert_equal({1=>nil, 2=>nil, 3=>nil}, [1,2,3].to_h)
